a6472614af767afe428e3560b0b76996ef120cd3
Taylor A Wong <taylorwong@lh-cslab-013.sandiego.edu>
2019-02-10 14:35:02 -0800

    workig on implementing ls and ls -al commands

--- ttsh.c
+++ ttsh.c
@@ -2,24 +2,37 @@
  * COMP 310 Operating Systems
  * Project 1: Torero Shell
  * Names: Taylor Wong and Mia Kim
+ *
+ * TODO Basic commands
+ * background/foreground
+ * CD and EXIT must work
+ * Pipes or IO redirection - one of the two by wednesday
  */
 
 #define _XOPEN_SOURCE 600
+#define PATH_MAX 50
 
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h> 
+#include <sys/stat.h>
 #include <sys/wait.h>
 #include <signal.h>
 #include "parse_args.h"
 #include "history_queue.h"
+#include <dirent.h>
+#include <time.h>
+#include <pwd.h>
+#include <grp.h>
 
 void child_handler(__attribute__ ((unused)) int sig);
 int builtin_cmd(char **argv);
 void execute_cmd(char **argv, int bg);
 extern char *pastCmd(char *cmdline);
+void printList(char *option);
+char* formatTime(struct stat attr);
 
 int main() { 
 	// Handle SIGCHILD signal by calling child_handler signal instead
@@ -44,7 +57,7 @@ int main() {
 			fflush(stdout);
 			exit(0);
 		}
-		fprintf(stdout, "DEBUG: %s\n", cmdline);
+		//fprintf(stdout, "DEBUG: %s\n", cmdline);
 		char *argv[MAXARGS];
 		// parseArguments returns non-zero (true) if the command line includes & at the end
 		// indicating that the command should be run in the background
@@ -65,7 +78,7 @@ int main() {
 		//add the command to the history queue and increment count
 		add_queue(cmdline,count);
 		count++;
-		}	
+	}	
 	return 0;
 }
 void child_handler(__attribute__ ((unused)) int sig){
@@ -109,7 +122,12 @@ int builtin_cmd(char **argv){
 	}
 	else if(strcmp(argv[0], "ls") == 0)
 	{
-		printList();
+		printList(argv[0]);
+		return 1;
+	}
+	else if(strcmp(argv[0], "ls -al") == 0)
+	{
+		printList(argv[0]);
 		return 1;
 	}
 	else
@@ -134,34 +152,94 @@ void execute_cmd(char **argv, int bg){
 	if(bg == 0){ // foreground
 		wpid = waitpid(pid, NULL, 0);
 		printf("%d \n", wpid);
-			//int status;
-			//while(wpid < 0)
-			//	wpid = waitpid(pid, &status, WUNTRACED);
+		//int status;
+		//while(wpid < 0)
+		//	wpid = waitpid(pid, &status, WUNTRACED);
 	}
 	//else // background
 	//	wpid = waitpid(pid, NULL, WNOHANG);
 }
 
 /*
- *
+ * ls command - lists the files within the current directory
  *
  */
-void printList()
+void printList(char *option)
 {
-		
+	printf("in Print List func\n"); 
+	//	char currentDir[PATH_MAX];
+	char *currentDir = NULL;	
+	currentDir = getenv("PWD");
+	if(NULL == currentDir) 
+	{
+		printf("ERROR couldn't get working directory\n");	
+	}
+	else
+	{
+		printf("Current directory %s\n", currentDir);
+		DIR *myDirectory;
+		struct dirent *dirEntry;
+		myDirectory = opendir(currentDir);
+		if(myDirectory != NULL)
+		{
+			struct stat attr;
+			char *time = NULL;
+			while((dirEntry = readdir(myDirectory)) != NULL)
+			{
+				if(strcmp(option, "ls") == 0) 
+				{
+					printf("%s\t", dirEntry->d_name);
+				}
+				else
+				{
+					stat(dirEntry->d_name, &attr);
+					time = ctime(&attr.st_mtime);
+					//time = formatTime(attr)
+					printf("%o %lu %s %s %ld %s %s\n", attr.st_mode, attr.st_ino, (getpwuid(attr.st_uid))->pw_name, (getgrgid(attr.st_gid))->gr_name, attr.st_size, time, dirEntry->d_name);
+				}
+			}
+		}
+		closedir(myDirectory);
+	}
 }
 
+char* formatTime(struct stat attr)
+{
+	char *timeStamp = NULL;
+	char *time = NULL;
+	char actualTime[50], *token, *token2;
+	char delim[] = " ";
+
+	timeStamp = ctime(&attr.st_mtime);
+	token = strtok(timeStamp, delim); // weekday
+	token = strtok(NULL, delim); // month
+	strcat(time, token);
+	strcat(time, " ");
+	token = strtok(NULL, delim); // day #
+	strcat(time, token);
+	strcat(time, " ");
+	token = strtok(NULL, delim); // time
+	token2 = strtok(token, ":"); // hour
+	strcat(actualTime, token2);
+	strcat(actualTime, ":");
+	token2 = strtok(NULL, ":"); // minutes
+	strcat(actualTime, token2);
+	strcat(time, actualTime);
+
+	printf("TIME IN FORMAT TIME FUNC %s\n", time);
+	return time;
+}
 /*
-void NumCmd(char **argv, int *count){
-	char cmdline[strlen(argv[0])];
-	// get rid of ! by shifting chars
-	int i;
-	for (i = 0; i < (int) strlen(argv[0]); i++){
-		cmdline[i] = argv[0][i+1];
-	}	
-	printf("cmdline= %s\n", cmdline);
-	unsigned int num = strtol(cmdline, NULL, 0);
-	
-	if(strcmp(
+   void NumCmd(char **argv, int *count){
+   char cmdline[strlen(argv[0])];
+// get rid of ! by shifting chars
+int i;
+for (i = 0; i < (int) strlen(argv[0]); i++){
+cmdline[i] = argv[0][i+1];
+}	
+printf("cmdline= %s\n", cmdline);
+unsigned int num = strtol(cmdline, NULL, 0);
+
+if(strcmp(
 }
 */
